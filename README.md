0628cf268a00149eba99aea03fe33588a852aac3
EMILIO <emilio@192.168.0.103>
2024-04-11 10:02:39 -0500

    Crando proyecto

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reto Frontend Angular
+
+<!-- [VER PUBLICADO](https://emeltec.github.io/reto-frontend-angular/) -->
+
+
+## Development server
+
+- npm install
+
+- npm run start
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
